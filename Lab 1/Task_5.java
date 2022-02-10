4e40b3b1857dcb08461dab80c074a71e177c808d
MrGriefer <15035598+MrGriefer@users.noreply.github.com>
2022-02-10 20:45:41 +0300

    Create Task_5.java

---
+++ Lab 1/Task_5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Task #5 - Create a program from scratch
+ */
+public class Mileage {
+
+    public static void main(String[] args) {
+        System.out.println("This program will calculate gas mileage in miles per gallon.");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the amount of miles driven: ");
+        double milesDriven = keyboard.nextDouble();
+
+        System.out.print("Enter the amount of gallons used: ");
+        double gallons = keyboard.nextDouble();
+
+        double milesPerGallon = milesDriven / gallons;
+        System.out.printf("Miles per gallon = %.2f", milesPerGallon);
+    }
+
+}
