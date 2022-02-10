dac431b347b6783b55a9dbab6fc7cf629f451ca3
MrGriefer <15035598+MrGriefer@users.noreply.github.com>
2022-02-10 17:34:56 +0300

    Update Task1.java

--- Lab 1/Task1.java
+++ Lab 1/Task1.java
@@ -1,1 +1,24 @@
+/**
+ *
+ * Task #1 - Correcting Logic Errors in Formulas
+ *
+ */
+public class NumericTypes {
 
+    public static void main(String[] args) {
+        final int NUMBER = 2;
+        double SCORE1 = 100;
+        int SCORE2 = 95;
+        final int BOILING_IN_F = 212;
+
+        double average = (SCORE1 + SCORE2) / NUMBER;
+        String output = SCORE1 + " and " + SCORE2 + " have an average of " + average;
+        System.out.println(output);
+
+        int fToC = (5 * (BOILING_IN_F - 32)) / 9;
+        output = BOILING_IN_F + " in Fahrenheit is " + fToC + " in Celsius.";
+        System.out.println(output);
+        System.out.println();
+    }
+
+}
