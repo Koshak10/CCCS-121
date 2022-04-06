b69b37ae4171f5d05e9285dabeb71598ff7cc146
MrGriefer <15035598+MrGriefer@users.noreply.github.com>
2022-04-06 17:51:50 +0300

    Update Song.java

--- Lab 6/Task 3/Song.java
+++ Lab 6/Task 3/Song.java
@@ -1,1 +1,30 @@
+/**
+ * This class stores data about a song.
+ */
+public class Song {
 
+    // The song's title
+    private String title;
+
+    // The song's artist
+    private String artist;
+
+    /**
+     * Constructor
+     * @param title A reference to a String object containing the song's title.
+     * @param artist A reference to a String object containing the song's artist.
+     */
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    /**
+     * The toString method
+     * @return A String object containing the name of the song and the artist.
+     */
+    @Override
+    public String toString() {
+        return title + " by " + artist + "\n";
+    }
+}
