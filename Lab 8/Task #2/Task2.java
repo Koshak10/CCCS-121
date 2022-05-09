c19314d25c6797e9ecfc5f130c1493d87f05075f
MrGriefer <15035598+MrGriefer@users.noreply.github.com>
2022-05-10 00:17:48 +0300

    Update Task2.java

--- Lab 8/Task #2/Task2.java
+++ Lab 8/Task #2/Task2.java
@@ -1,7 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task2 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File file = new File("secret.txt");
+        Scanner inputFile = new Scanner(file);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (inputFile.hasNext()) {
+            String str = inputFile.nextLine();
+            String[] tokens = str.split(" ");
+
+            for (int i = 0; i < tokens.length; i++) {
+                if (i % 5 == 0) {
+                    stringBuilder.append(tokens[i].toUpperCase().charAt(0));
+                }
+            }
+
+        }
+
+        System.out.println(stringBuilder);
+        inputFile.close();
     }
 
 }
