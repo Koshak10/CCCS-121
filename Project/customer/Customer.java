cefda86502fbf8cac6446b8a50734107673b5086
MrGriefer <15035598+MrGriefer@users.noreply.github.com>
2022-05-16 19:19:48 +0300

    Create Customer.java

---
+++ Project/customer/Customer.java
@@ -0,0 +1,44 @@
+package customer;
+
+public class Customer {
+
+    private int customerId;
+    private String name;
+    private String phoneNumber;
+
+    public Customer() {}
+
+    public Customer(int customerId, String name, String phoneNumber) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return customerId + ", " + name + ", " + phoneNumber;
+    }
+}
